396398ee896c6e516554f07807a7b58ce1a8dffd
proraj9088 <rp8735685@gmail.com>
2021-07-21 13:27:48 +0530

    styled chat bg

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,45 +1,78 @@
+  
 const socket = io();
 
-async function loadMsgs(){
-     
-  const allMsgs =   await axios.get('/allmessages');
+async function loadMsgs() {
+  const allMsgs = await axios.get("/allmessages");
+  console.log(allMsgs);
 
-  for(let msg of allMsgs.data) {
-    $('#all-msg-container').append(` <li>
-       <span>${msg.user}</span>
-       <span>${msg.createdAt}</span>
-       <p>${msg.content}</p>
-    </li>`);
-  }
+  for (let msg of allMsgs.data) {
 
-}
+    const time = timeDifference(new Date(),new Date(msg.createdAt))
 
- loadMsgs();
-
-
-$('#send-msg-btn').click(()=> {
-     
-    const textMsg = $('#msg-text').val();
-
-    socket.emit('send-msg', {
-        user: currentUser,
-        msg: textMsg
-    })
-
-
-    $('#msg-text').val("");
-    location.reload();
-})
-
-
-socket.on("recived-msg", (data) => {
     $("#all-msg-container").append(
       `<li>
-          <span>${data.user}</span>
-          <span>${data.createdAt}</span>
-          <p>${data.msg}</p>
-        </li>`
+        <span>${msg.user}</span>
+        <p>${msg.content}</p>
+        <div>
+          <span>${time}</span>
+        </div>
+      </li>`
     );
+  }
+}
 
+loadMsgs();
+
+$("#send-msg-btn").click(() => {
+  const textMsg = $("#msg-text").val();
+
+  socket.emit("send-msg", {
+    user: currentUser,
+    msg: textMsg,
   });
 
+  $("#msg-text").val("");
+  location.reload();
+});
+
+socket.on("recived-msg", (data) => {
+  
+  const time = timeDifference(new Date(),new Date(data.createdAt))
+  
+  $("#all-msg-container").append(
+    `<li>
+        <span>${data.user}</span>
+        <span>${time}</span>
+        <p>${data.msg}</p>
+      </li>`
+  );
+});
+
+
+function timeDifference(current, previous) {
+  var msPerMinute = 60 * 1000;
+  var msPerHour = msPerMinute * 60;
+  var msPerDay = msPerHour * 24;
+  var msPerMonth = msPerDay * 30;
+  var msPerYear = msPerDay * 365;
+
+  var elapsed = current - previous;
+
+  if (elapsed < msPerMinute) {
+    if (elapsed / 1000 < 30) {
+      return "Just now";
+    }
+
+    return Math.round(elapsed / 1000) + " seconds ago";
+  } else if (elapsed < msPerHour) {
+    return Math.round(elapsed / msPerMinute) + " minutes ago";
+  } else if (elapsed < msPerDay) {
+    return Math.round(elapsed / msPerHour) + " hours ago";
+  } else if (elapsed < msPerMonth) {
+    return Math.round(elapsed / msPerDay) + " days ago";
+  } else if (elapsed < msPerYear) {
+    return Math.round(elapsed / msPerMonth) + " months ago";
+  } else {
+    return Math.round(elapsed / msPerYear) + " years ago";
+  }
+}
